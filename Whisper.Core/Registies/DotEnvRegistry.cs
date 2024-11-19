31f84f6235c9dfc022c170b0b9a7ae7afb806451
d.ushkan <ushkndn@gmail.com>
2024-11-19 21:13:22 +0300

    feat: setup for env variables and env file(.env)

---
+++ Whisper.Core/Registies/DotEnvRegistry.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Whisper.Core.Registies;
+
+public class DotEnvRegistry(IHostEnvironment env)
+{
+    public DotEnvRegistry AddDotEnvConfiguration(IConfigurationBuilder configBuilder)
+    {
+        var name = env.EnvironmentName;
+        configBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{name}.json", optional: true, reloadOnChange: true)
+                     .AddEnvironmentVariables("OCT_");
+        return this;
+    }
+}
