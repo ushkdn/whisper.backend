48b9030832f8bc206e33c752cd8d15b23fcbbc8a
d.ushkan <ushkndn@gmail.com>
2024-11-27 20:52:42 +0300

    feat: swagger setup w/ launch url

---
+++ Whisper/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Whisper.User.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UserController : Controller
+{
+    [HttpPost]
+    public string Init([FromBody] string test)
+    {
+        return test + " initial";
+    }
+}
--- Whisper/Program.cs
+++ Whisper/Program.cs
@@ -6,18 +6,17 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
         builder.Services.AddOpenApi();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
