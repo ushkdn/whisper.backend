6960ac4aadff0f8559625669d1bb47f247f6d2d5
ushkdn <ushkndn@gmail.com>
2024-11-29 06:20:38 +0300

    fix: migrations from another projects

--- Whisper.Data/WhisperDbContext.cs
+++ Whisper.Data/WhisperDbContext.cs
@@ -6,31 +6,6 @@ using Whisper.Data.Entities;
 
 namespace Whisper.Data;
 
-
-public class WhisperDbContextFactory : IDesignTimeDbContextFactory<WhisperDbContext>
-{
-    public WhisperDbContext CreateDbContext(string[] args)
-    {
-        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../Whisper.User");
-
-        if (!Directory.Exists(basePath))
-        {
-            throw new DirectoryNotFoundException($"Base path not found: {basePath}");
-        }
-
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(basePath)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("Postgres");
-
-        var optionsBuilder = new DbContextOptionsBuilder<WhisperDbContext>();
-        optionsBuilder.UseNpgsql(connectionString);
-
-        return new WhisperDbContext(optionsBuilder.Options);
-    }
-}
 public class WhisperDbContext(DbContextOptions options) : DbContext(options)
 {
     internal DbSet<UserEntity> Users { get; set; }
